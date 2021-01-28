750b694c22744e109a66dc279393d8b4cc324820
incapaz <60857303+zapacni@users.noreply.github.com>
2021-01-28 02:32:32 -0800

    Aver si ya funcionaaaaaaaa

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import os
 import discord
 from discord.ext import commands
 
-client = discord.Client()
+client = commands.Bot(command_prefix = "dpy!")
 
 @client.event
 async def on_ready():
     print("listo xd")
 
-client.run(os.environ["BOT_TOKEN"])
+client.run(os.getenv("BOT_TOKEN"))

747442a3e257e8d7bc2a87c523b4914b8e78d1ff
incapaz <60857303+zapacni@users.noreply.github.com>
2021-02-02 23:26:18 -0800

    arreglar los comandos xd

--- main.py
+++ main.py
@@ -50,10 +50,10 @@ async def eliminar(ctx, cantidad):
 
 @client.command()
 async def convertir_a_robux(ctx, usd):
-    await ctx.send(f"USD ${usd} son <:robux:806422545359306752> {usd/0.0035}")
+    await ctx.send(f"USD ${usd} son <:robux:806422545359306752> {float(usd)/0.0035}")
 
 @client.command()
 async def convertir_a_usd(ctx, robux):
-    await ctx.send(f"<:robux:806422545359306752> {robux} son USD ${robux*0.0035}")
+    await ctx.send(f"<:robux:806422545359306752> {robux} son USD ${int(robux)*0.0035}")
 
 client.run(os.getenv("BOT_TOKEN"))
